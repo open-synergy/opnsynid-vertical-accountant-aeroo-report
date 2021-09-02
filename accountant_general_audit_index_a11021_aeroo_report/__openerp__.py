e2014276274feaadc70b4015d5dc05c32ad2548a
Nurazmi <azmimr67@gmail.com>
2021-09-02 11:08:41 +0700

    [8.0.1.0.0] accountant_general_audit_index_a11021_aeroo_report

---
+++ accountant_general_audit_index_a11021_aeroo_report/__openerp__.py
@@ -0,0 +1,25 @@
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author/
+{
+    "name": "General Audit Index A.1102.1 Aeroo Report",
+    "version": "8.0.1.0.0",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "website": "https://simetri-sinergi.id",
+    "depends": [
+        "accountant_general_audit_index_a11021",
+        "report_aeroo",
+        "base_print_policy",
+    ],
+    "data": [
+        "reports/report_general_audit_index_a11021.xml",
+    ],
+    "images": [
+        "static/description/banner.png",
+    ],
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+    "license": "AGPL-3",
+}
