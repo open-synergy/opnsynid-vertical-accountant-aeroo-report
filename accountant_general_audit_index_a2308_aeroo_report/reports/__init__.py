20a53054f801ca676c696bdfc87311c7fc7f3d45
Nurazmi <azmimr67@gmail.com>
2021-10-29 11:20:07 +0700

    [8.0.1.0.0] accountant_general_audit_index_a2308_aeroo_report

---
+++ accountant_general_audit_index_a2308_aeroo_report/reports/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 201521penSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from . import (
+    parser_index_a2308,
+)
